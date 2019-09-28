2395bf1229336798f17f776b5fab3ebd85a35b93
amanj <amanj.2013bz@gmail.com>
2019-09-28 13:38:41 +0330

    First Commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta content="کارآفرینی , مریوان , استارتاپ , زریبار , شرکت هلدینگ , برگزاری همایش , استارتاپ کردستان , بازاریابی , کاریابی" name="keywords">
+    <meta content="وب سایت شرکت کارآفرینی تاوژین در شهرستان مریوان" name="description">
+    <title>گروه کارآفرینی تاوژین</title>
+    @include('partials.csslinks')
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="21388bad-4040-4577-97fa-fb057b4fb30c";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+</head>
+<body data-spy="scroll" data-target="#main-navbar">
+@yield('content')
+
+@include('partials.scripts')
+@yield('scripts')
+</body>
+</html>
---
+++ resources/views/partials/csslinks.blade.php
@@ -0,0 +1,31 @@
+<!-- Favicons -->
+<link href="img/favicon.png" rel="icon">
+<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/iranian-sans" type="text/css"/>
+
+<!-- Bootstrap CSS File -->
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Libraries CSS Files -->
+<link href="lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+<link href="lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+<link href="lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="lib/animate/animate.min.css" rel="stylesheet">
+<link href="lib/venobox/venobox.css" rel="stylesheet">
+
+<!-- Nivo Slider Theme -->
+<link href="css/nivo-slider-theme.css" rel="stylesheet">
+
+<!-- Main Stylesheet File -->
+<link href="css/style.css" rel="stylesheet">
+
+<!-- Responsive Stylesheet File -->
+<link href="css/responsive.css" rel="stylesheet">
+
+
+
+
+
